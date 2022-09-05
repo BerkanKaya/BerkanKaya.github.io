82b4fd8ec2649f4a98fa6e2fb66c380b2846d42b
BerkanKaya <99059255@mydavinci.nl>
2022-09-05 14:50:47 +0200

    Phone Homepage (Responsive)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>BerkanPortfolio</title>
+        <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/general.css') }}" rel="stylesheet">
+        <link rel="icon" href="{{asset('images/berkan_logowit.png')}}" type="image/x-icon">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/e0cf27849f.js" crossorigin="anonymous"></script>
+
+        <style>
+            body {
+                background-color: #111;
+            }
+        </style>
+    </head>
+<body>
+    <div class="navbar"> 
+        <div class="icons">
+        <i  id="instaLogo"class="fa fa-instagram"></i>
+        <i  id="linkedInLogo"class="fa fa-linkedin"></i>
+        <i id="githubLogo"class="fa fa-github"></i>
+        </div>
+        <a href="#">Werk</a>
+        <a href="#">Contact</a>
+    </div>
+
+    <p class="intro">Hoi. Ik ben Berkan. Een Software Developer </p>
+
+    <p class="description">Ik ben een erg enthiousaste student wanneer het komt op Web Development en Design. En streef altijd tot perfectionisme</p>
+
+    <div class="logoBackground">
+    <img src="images/berkan_logowit.png" class="logo">
+    </div>
+
+</body>
